9b90dc534bb9bdcbea2babb87db1848bb3a21284
Amit Assa <amitassa9@gmail.com>
2022-12-10 12:52:34 +0200

    student list row activity implemented

--- app/src/main/java/amit/myapp/studentsapp/StudentListActivity.java
+++ app/src/main/java/amit/myapp/studentsapp/StudentListActivity.java
@@ -3,6 +3,7 @@ package amit.myapp.studentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class StudentListActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class StudentListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
+
+        ListView studentsList = findViewById(R.id.studentlist_list);
+
     }
 }
