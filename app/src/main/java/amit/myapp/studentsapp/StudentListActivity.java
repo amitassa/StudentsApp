7a8c3f5be00ae283e5cabdabce5ab7be09e02f8b
Amit Assa <amitassa9@gmail.com>
2022-12-10 13:13:57 +0200

    basic student list adapter

--- app/src/main/java/amit/myapp/studentsapp/StudentListActivity.java
+++ app/src/main/java/amit/myapp/studentsapp/StudentListActivity.java
@@ -3,7 +3,12 @@ package amit.myapp.studentsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class StudentListActivity extends AppCompatActivity {
 
@@ -13,6 +18,35 @@ public class StudentListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_list);
 
         ListView studentsList = findViewById(R.id.studentlist_list);
+        studentsList.setAdapter(new StudentListAdapter());
+    }
 
+    class StudentListAdapter extends BaseAdapter{
+
+//        public StudentListAdapter(Context ctx, )
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int pos, View view, ViewGroup viewGroup) {
+            if (view == null){
+                view = getLayoutInflater().inflate(R.layout.student_list_row, null);
+            }
+            TextView nameTv = view.findViewById(R.id.studentlistrow_name_textView);
+            nameTv.setText("Name "+ pos);
+            return view;
+        }
     }
 }
