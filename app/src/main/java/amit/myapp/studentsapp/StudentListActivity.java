4f736b04a7df48195552dd1e5f37b51439f106cc
Amit Assa <amitassa9@gmail.com>
2022-12-10 12:35:56 +0200

    activities added

---
+++ app/src/main/java/amit/myapp/studentsapp/StudentListActivity.java
@@ -0,0 +1,14 @@
+package amit.myapp.studentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_list);
+    }
+}
---
+++ app/src/main/java/amit/myapp/studentsapp/activity_add_student.java
@@ -0,0 +1,14 @@
+package amit.myapp.studentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_add_student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+    }
+}
