0dd1c248e9f4fe0f82e353c8cc8344223f84f8f2
Amit Assa <amitassa9@gmail.com>
2022-12-31 14:02:13 +0200

    add & remove fragments

--- app/src/main/java/amit/myapp/studentsapp/MainActivity.java
+++ app/src/main/java/amit/myapp/studentsapp/MainActivity.java
@@ -1,8 +1,15 @@
 package amit.myapp.studentsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import amit.myapp.studentsapp.model.BlueFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.main_add_button);
+        Button removeBtn = (Button) findViewById(R.id.main_remove_button);
+
+        BlueFragment myFrag = new BlueFragment();
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFrag(myFrag);
+            }
+        });
+
+        removeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeFrag(myFrag);
+            }
+        });
+
+
+    }
+
+    void addFrag(Fragment frag){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction tran = manager.beginTransaction();
+        tran.add(R.id.main_frag_container, frag);
+        tran.addToBackStack("someTAG");
+        tran.commit();
+    }
+
+    void removeFrag(Fragment frag){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction tran = manager.beginTransaction();
+        tran.remove(frag);
+        tran.addToBackStack("someTAG1");
+        tran.commit();
     }
 }
