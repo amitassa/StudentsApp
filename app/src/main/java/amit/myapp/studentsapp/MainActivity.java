fea9c9bfcbb6dbca6930a8545f5f71826047586a
Amit Assa <amitassa9@gmail.com>
2023-01-07 11:34:54 +0200

    navigation bar ui implemented

--- app/src/main/java/amit/myapp/studentsapp/MainActivity.java
+++ app/src/main/java/amit/myapp/studentsapp/MainActivity.java
@@ -1,24 +1,38 @@
 package amit.myapp.studentsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import amit.myapp.studentsapp.model.BlueFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            navController.popBackStack();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
